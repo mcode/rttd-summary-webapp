e4b89ebf9131adc9e4810b4c7d133add7e659242
Dylan Phelan <dtphelan1@gmail.com>
2022-02-02 16:11:44 -0500

    break on words

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -5,7 +5,7 @@ function TableData({ isFirstCol, data }) {
     <td
       className={`${
         isFirstCol ? "w-48 font-medium" : "w-96 font-light"
-      } px-6 py-3 text-sm text-gray-900 break-normal`}
+      } px-6 py-3 text-sm text-gray-900 break-words`}
     >
       {dataDisplay(data)}
     </td>
