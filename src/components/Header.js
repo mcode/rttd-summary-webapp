df80d317e3547bbf91573c72f515380b45416b32
Dylan Phelan <dtphelan1@gmail.com>
2022-02-07 11:05:04 -0500

    header has max-width now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import logo from "../codex-logo.png";
 function Header() {
   return (
-    <div className="bg-slate-600 w-screen">
+    <div className="bg-slate-600 w-screen max-w-full">
       <header className="container sm:mx-auto px-4 sm:px-0 h-24 flex justify-between items-center text-lg">
         <p className="text-white text-lg md:text-2xl ">
           Radiation Therapy Treatment Data Summary
